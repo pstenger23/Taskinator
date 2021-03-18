606979ad8422d6ad80661125eb398d1b9bd45ebd
P Stenger <pstenger23@gmail.com>
2021-03-17 19:12:57 -0500

    form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
 
 var createTaskHandler = function() {
     
+    event.preventDefault();
+
     var listedItemEl = document.createElement("li");
     listedItemEl.className = "task-item";
     listedItemEl.textContent = "This is a new task.";
@@ -10,6 +13,6 @@ var createTaskHandler = function() {
 
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
